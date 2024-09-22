22d2e3acec0f3456f30304d964e49f969e4c09f5
Daniil <daniiljmuda@gmail.com>
2024-09-22 13:21:47 +0300

    frst111

---
+++ extra_files/brain_geometry_progression.py
@@ -0,0 +1,8 @@
+from games import geometry_progression
+from extra_files.brain_game_launcher import launch_game
+
+def main():
+    launch_game(geometry_progression)
+
+if __name__ == '__main__':
+    main()
---
+++ extra_files/brain_nok.py
@@ -0,0 +1,8 @@
+from games import nok
+from extra_files.brain_game_launcher import launch_game
+
+def main():
+    launch_game(nok)
+
+if __name__ == '__main__':
+    main()
